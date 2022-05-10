5623d59172777c6ce4312d18dd9da7fd99fa46b4
jumaBrian <njengabrianj@gmail.com>
2022-05-10 09:37:16 +0300

    ft: Add form to register species in the area

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -13,9 +13,16 @@ public class App {
         staticFileLocation("/public");
         staticFileLocation("/public");      // EnableDebugScreen();
 
+        // index page
         get("/", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
             return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        // animals registry
+        get("/animals-form", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "animals.hbs");
         }, new HandlebarsTemplateEngine());
     }
 }
